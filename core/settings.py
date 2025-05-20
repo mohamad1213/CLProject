3dfc7d233c7703d806b287ac456226f5db0ad46d
mohamad1213 <hatami391998@gmail.com>
2025-05-20 23:17:54 +0700

    updaye

--- core/settings.py
+++ core/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS =  os.environ.get("ALLOWED_HOSTS", "localhost").split(",")
+ALLOWED_HOSTS =  os.environ.get("ALLOWED_HOSTS", "*").split(",")
 # ALLOWED_HOSTS = ['*', '.vercel.app']
 
 # Application definition
